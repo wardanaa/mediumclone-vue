6916fbde00f2d9c317a0f21c7b5b9520c93ab7d6
Made Wardana <made.wardana44@gmail.com>
2022-10-21 22:03:16 +0800

    04-updating-vue-to-version-3

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>Medium clone</h1>
+  <div>
+    <h1>Medium clone!</h1>
+
+    <router-view></router-view>
+  </div>
 </template>
 
-<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,18 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import {createRouter, createWebHistory} from 'vue-router'
 
-Vue.use(VueRouter)
+import Home from '@/views/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
-const router = new VueRouter({
-  routes
+const router = createRouter({
+  routes,
+  history: createWebHistory()
 })
 
 export default router
